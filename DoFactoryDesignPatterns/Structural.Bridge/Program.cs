150a6460c00c8868fa5c4b46374025eb6bbda551
ChrisJ <jiangl_8809@hotmail.com>
2014-10-29 10:40:15 +0800

    Examples for Bridge pattern

---
+++ DoFactoryDesignPatterns/Structural.Bridge/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Definition: Decouple an abstraction from its implementation so that the two can vary independently. 
+ */
+
+namespace Structural.Bridge
+{
+	using System;
+
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	Abstraction abs = new RefinedAbstraction();
+		//	Implementor impA = new ConcreteImplementorA();
+		//	abs.Implementor = impA;
+		//	abs.Operation();
+
+		//	Implementor impB = new ConcreteImplementorB();
+		//	abs.Implementor = impB;
+		//	abs.Operation();
+
+		//	Console.ReadKey();
+		//}
+	}
+
+	/// <summary>
+	/// Abstraction(BusinessObject)
+	/// 1. defines the abstraction's interface.
+	/// 2. maintains a reference to an object of type Implementor. 
+	/// </summary>
+	public abstract class Abstraction
+	{
+		protected Implementor implementor;
+
+		public Implementor Implementor
+		{
+			//private get { return implementor; }
+			set { implementor = value; }
+		}
+
+		public virtual void Operation()
+		{
+			implementor.Operation();
+		}
+	}
+
+	/// <summary>
+	/// RefinedAbstraction   (CustomersBusinessObject)
+	/// 1. extends the interface defined by Abstraction. 
+	/// </summary>
+	public class RefinedAbstraction : Abstraction
+	{
+		public override void Operation()
+		{
+			base.Operation();
+		}
+	}
+
+	/// <summary>
+	/// Implementor   (DataObject)
+	/// 1. defines the interface for implementation classes. This interface doesn't have to correspond exactly to Abstraction's interface;
+	///		in fact the two interfaces can be quite different. Typically the Implementation interface provides only primitive operations,
+	///		and Abstraction defines higher-level operations based on these primitives. 
+	/// </summary>
+	public abstract class Implementor
+	{
+		public abstract void Operation();
+	}
+
+	/// <summary>
+	/// ConcreteImplementor   (CustomersDataObject)
+	/// 1. implements the Implementor interface and defines its concrete implementation.
+	/// </summary>
+	public class ConcreteImplementorA : Implementor
+	{
+		public override void Operation()
+		{
+			Console.WriteLine("ConcreteImplementorA Operation");
+		}
+	}
+
+	public class ConcreteImplementorB : Implementor
+	{
+		public override void Operation()
+		{
+			Console.WriteLine("ConcreteImplementorB Operation");
+		}
+	}
+}
---
+++ DoFactoryDesignPatterns/Structural.Bridge/RealWorld.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Bridge
+{
+	class RealWorld
+	{
+		static void Main(string[] args)
+		{
+			Customers customers = new Customers("Chicago");
+
+			customers.DataObject = new CustomersData();
+
+			customers.Show();
+			customers.Next();
+			customers.Show();
+			customers.Next();
+			customers.Show();
+			customers.Add("Henry Velasquez");
+
+			customers.ShowAll();
+
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Abstraction' class
+	/// **could be an abstract class if it is inherited.
+	/// </summary>
+	public class CustomersBase
+	{
+		private DataObject _dataObject;
+
+		public DataObject DataObject
+		{
+			get { return _dataObject; }
+			set { _dataObject = value; }
+		}
+
+		protected string group;
+
+		public CustomersBase(string group)
+		{
+			this.group = group;
+		}
+
+		public virtual void Next()
+		{
+			_dataObject.NextRecord();
+		}
+
+		public virtual void Prior()
+		{
+			_dataObject.PriorRecord();
+		}
+
+		public virtual void Add(string customer)
+		{
+			_dataObject.AddRecord(customer);
+		}
+
+		public virtual void Delete(string customer)
+		{
+			_dataObject.DeleteRecord(customer);
+		}
+
+		public virtual void Show()
+		{
+			_dataObject.ShowRecord();
+		}
+
+		public virtual void ShowAll()
+		{
+			Console.WriteLine("Customer Group: " + this.group);
+			_dataObject.ShowAllRecord();
+		}
+	}
+
+	/// <summary>
+	/// The 'RefinedAbstraction' class
+	/// </summary>
+	public class Customers : CustomersBase
+	{
+		public Customers(string group)
+			: base(group)
+		{
+		}
+
+		public override void ShowAll()
+		{
+			Console.WriteLine();
+			Console.WriteLine("-------------------");
+			base.ShowAll();
+			Console.WriteLine("-------------------");
+		}
+	}
+	/// <summary>
+	/// The 'Implememtor' abstract class.
+	/// </summary>
+	public abstract class DataObject
+	{
+		public abstract void NextRecord();
+		public abstract void PriorRecord();
+		public abstract void AddRecord(string name);
+		public abstract void DeleteRecord(string name);
+		public abstract void ShowRecord();
+		public abstract void ShowAllRecord();
+	}
+
+	public class CustomersData : DataObject
+	{
+		private List<string> _customers = new List<string>();
+		private int _current = 0;
+
+		public CustomersData()
+		{
+			_customers.Add("Jim Jones");
+			_customers.Add("Samual Jackson");
+			_customers.Add("Allen Good");
+			_customers.Add("Ann Stills");
+			_customers.Add("Lisa Giolani");
+		}
+
+		public override void NextRecord()
+		{
+			if (_current <= _customers.Count() - 1)
+			{
+				_current++;
+			}
+		}
+
+		public override void PriorRecord()
+		{
+			if (_current > 0)
+			{
+				_current--;
+			}
+		}
+
+		public override void AddRecord(string customer)
+		{
+			_customers.Add(customer);
+		}
+
+		public override void DeleteRecord(string customer)
+		{
+			_customers.Remove(customer);
+		}
+
+		public override void ShowRecord()
+		{
+			Console.WriteLine(_customers[_current]);
+		}
+
+		public override void ShowAllRecord()
+		{
+			foreach (string customer in _customers)
+			{
+				Console.WriteLine(" " + customer);
+			}
+		}
+	}
+}
