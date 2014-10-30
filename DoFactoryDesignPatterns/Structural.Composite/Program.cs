214c3fa0484a2325708800266c6f01f2c01b4a51
ChrisJ <jiangl_8809@hotmail.com>
2014-10-30 12:48:32 +0800

    Examples for Composite pattern

---
+++ DoFactoryDesignPatterns/Structural.Composite/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Composite
+{
+	/// <summary>
+	/// Client  (CompositeApp)
+	/// 1. manipulates objects in the composition through the Component interface.
+	/// </summary>
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	Composite root = new Composite("root");
+		//	root.Add(new Leaf("Leaf A"));
+		//	root.Add(new Leaf("Leaf B"));
+
+		//	Composite comp = new Composite("Composite X");
+		//	comp.Add(new Leaf("Leaf XA"));
+		//	comp.Add(new Leaf("Leaf XB"));
+
+		//	root.Add(comp);
+		//	root.Add(new Leaf("Leaf C"));
+
+		//	Leaf leaf = new Leaf("Leaf D");
+		//	root.Add(leaf);
+
+		//	root.Display(1);
+
+		//	Console.WriteLine("------Romved Leaf D------");
+		//	root.Remove(leaf);
+		//	root.Display(1);
+
+		//	Console.ReadKey();
+		//}
+	}
+
+	/// <summary>
+	/// Component   (DrawingElement)
+	/// 1. declares the interface for objects(leaf and composite) in the composition
+	/// 2. implements default behavior for the interface common to all classes, as appropriate.
+	/// 3. declares an interface for accessing and manaaging its child components.
+	/// 4. (optional) defines an interface for accessing a component's parent in the recursive structure, and implements it if that's appropriate.
+	/// </summary>
+	public abstract class Component
+	{
+		protected string name;
+
+		public Component(string name)
+		{
+			this.name = name;
+		}
+
+		public abstract void Add(Component component);
+		public abstract void Remove(Component componenet);
+		public abstract void Display(int depth);
+	}
+
+	/// <summary>
+	/// Composite   (CompositeElement)
+	/// 1. defines behavior for components having children.
+	/// 2. stores child components.
+	/// 3. implements child-related operations in the Component interface.
+	/// </summary>
+	public class Composite : Component
+	{
+		private List<Component> _children = new List<Component>();
+
+		public Composite(string name)
+			: base(name)
+		{
+		}
+
+		public override void Add(Component component)
+		{
+			_children.Add(component);
+		}
+
+		public override void Remove(Component component)
+		{
+			_children.Remove(component);
+		}
+
+		public override void Display(int depth)
+		{
+			Console.WriteLine(new String('-', depth) + name);
+			foreach (Component item in _children)
+			{
+				item.Display(depth + 2);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Leaf   (PrimitiveElement)
+	/// 1. represents leaf objects in the composition. A leaf has no children.
+	/// 2. defines behavior for primitive objects in the composition.
+	/// </summary>
+	public class Leaf : Component
+	{
+		public Leaf(string name)
+			: base(name)
+		{
+		}
+
+		public override void Add(Component component)
+		{
+			Console.WriteLine("Can't be added to a leaf.");
+		}
+
+		public override void Remove(Component componenet)
+		{
+			Console.WriteLine("Can't be romved from a leaf.");
+		}
+
+		public override void Display(int depth)
+		{
+			Console.WriteLine(new String('-', depth) + name);
+		}
+	}
+}
---
+++ DoFactoryDesignPatterns/Structural.Composite/RealWorld.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Composite
+{
+	class RealWorld
+	{
+		static void Main(string[] args)
+		{
+			// Create a tree structure
+
+			CompositeElement root = new CompositeElement("Picture");
+			root.Add(new PrimitiveElement("Red Line"));
+			root.Add(new PrimitiveElement("Blue Circle"));
+			root.Add(new PrimitiveElement("Green Box"));
+
+			// Create a branch
+			CompositeElement comp = new CompositeElement("Two Circles");
+			comp.Add(new PrimitiveElement("Black Circle"));
+			comp.Add(new PrimitiveElement("White Circle"));
+			root.Add(comp);
+
+			// Add and remove a PrimitiveElement
+			PrimitiveElement pe = new PrimitiveElement("Yellow Line");
+			root.Add(pe);
+			root.Remove(pe);
+
+			// Recursively display nodes
+			root.Display(1);
+
+			// Wait for user
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Component' Treenode.
+	/// </summary>
+	public abstract class DrawingElement
+	{
+		protected string _name;
+
+		public DrawingElement(string name)
+		{
+			this._name = name;
+		}
+
+		public abstract void Add(DrawingElement de);
+		public abstract void Remove(DrawingElement de);
+		public abstract void Display(int indent);
+	}
+
+	/// <summary>
+	/// The 'Composite' class.
+	/// </summary>
+	public class CompositeElement : DrawingElement
+	{
+		private List<DrawingElement> childElements = new List<DrawingElement>();
+
+		public CompositeElement(string name)
+			: base(name)
+		{
+		}
+
+		public override void Add(DrawingElement de)
+		{
+			childElements.Add(de);
+		}
+
+		public override void Remove(DrawingElement de)
+		{
+			childElements.Remove(de);
+		}
+
+		public override void Display(int indent)
+		{
+			Console.WriteLine(new String('-', indent) + "+" + _name);
+
+			foreach (DrawingElement item in childElements)
+			{
+				item.Display(indent + 2);
+			}
+		}
+	}
+
+	/// <summary>
+	/// The 'Leaf' class.
+	/// </summary>
+	public class PrimitiveElement : DrawingElement
+	{
+		public PrimitiveElement(string name)
+			: base(name)
+		{
+		}
+
+		public override void Add(DrawingElement de)
+		{
+			throw new NotImplementedException("Can't be added to a PrimitiveElement.");
+		}
+
+		public override void Remove(DrawingElement de)
+		{
+			throw new NotImplementedException("Can't be removed from a PrimitiveElement");
+		}
+
+		public override void Display(int indent)
+		{
+			Console.WriteLine(new String('-', indent) + this._name);
+		}
+	}
+}
