e9c4200e0334013b18f65d2ed670580d0a2a2e8b
ChrisJ <jiangl_8809@hotmail.com>
2014-10-31 10:29:07 +0800

    Examples for Decorator pattern

---
+++ DoFactoryDesignPatterns/Structural.Decorator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Structural.Decorator
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			ConcreteComponent c = new ConcreteComponent();
+//			ConcreteDecoratorA da = new ConcreteDecoratorA();
+//			ConcreteDecoratorB db = new ConcreteDecoratorB();
+
+//			da.SetComponent(c);
+//			da.Operation();
+
+//			Console.WriteLine("------------");
+//			//db.SetComponent(da);
+//			//db.Operation();
+
+//			//da.SetComponent(db);
+//			//da.Operation();
+
+//			da.SetComponent(da);
+//			da.Operation();
+//			Console.ReadKey();
+//		}
+//	}
+
+//	/// <summary>
+//	/// Component   (LibraryItem)
+//	/// 1. defines the interface for objects that can have responsibilities added to them dynamically. 
+//	/// </summary>
+//	public abstract class Component
+//	{
+//		public abstract void Operation();
+//	}
+
+//	/// <summary>
+//	/// ConcreteComponent   (Book, Video)
+//	/// 1. defines an object to which additional responsibilities can be attached. 
+//	/// </summary>
+//	public class ConcreteComponent : Component
+//	{
+//		public override void Operation()
+//		{
+//			Console.WriteLine("ConcreteComponent.Operation");
+//		}
+//	}
+
+//	/// <summary>
+//	/// Decorator   (Decorator)
+//	/// 1. maintains a reference to a Component object and defines an interface that conforms to Component's interface. 
+//	/// </summary>
+//	public abstract class Decorator : Component
+//	{
+//		protected Component component;
+//		public void SetComponent(Component component)
+//		{
+//			this.component = component;
+//		}
+
+//		public override void Operation()
+//		{
+//			if (this.component != null)
+//			{
+//				component.Operation();
+//			}
+//		}
+//	}
+
+//	/// <summary>
+//	/// ConcreteDecorator   (Borrowable)
+//	/// 1. adds responsibilities to the component. 
+//	/// </summary>
+//	public class ConcreteDecoratorA : Decorator
+//	{
+//		public override void Operation()
+//		{
+//			base.Operation();
+//			Console.WriteLine("ConcreteDecoratorA.Operation");
+//		}
+//	}
+
+//	public class ConcreteDecoratorB : Decorator
+//	{
+//		public override void Operation()
+//		{
+//			base.Operation();
+//			Console.WriteLine("ConcreteDecoratorB.Operation");
+//		}
+//	}
+//}
---
+++ DoFactoryDesignPatterns/Structural.Decorator/RealWorld.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Decorator
+{
+	class RealWorld
+	{
+		public static void Main()
+		{
+			// Create book
+			Book book = new Book("Worley", "Inside ASP.NET", 10);
+			book.Display();
+
+			// Create video
+			Video video = new Video("Spielberg", "Jaws", 23, 92);
+			video.Display();
+
+			// Make video borrowable, then borrow and display
+			Console.WriteLine("\nMaking video borrowable:");
+
+			Borrowable borrowvideo = new Borrowable(video);
+			borrowvideo.BorrowItem("Customer #1");
+			borrowvideo.BorrowItem("Customer #2");
+			
+			borrowvideo.Display();
+			
+			// Wait for user
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Component' abstract class.
+	/// </summary>
+	public abstract class LibraryItem
+	{
+		private int _numCopies;
+
+		public int NumCopies
+		{
+			get { return _numCopies; }
+			set { _numCopies = value; }
+		}
+
+		public abstract void Display();
+	}
+
+	/// <summary>
+	/// The 'ConcreteComponent' class.
+	/// </summary>
+	public class Book : LibraryItem
+	{
+		public string Author { get; set; }
+		public string Title { get; set; }
+
+		public Book(string author, string title, int numCopies)
+		{
+			this.Author = author;
+			this.Title = title;
+			this.NumCopies = numCopies;
+		}
+
+		public override void Display()
+		{
+			Console.WriteLine("\nBook ------");
+			Console.WriteLine(" Author: {0}", this.Author);
+			Console.WriteLine(" Title: {0}", this.Title);
+			Console.WriteLine(" # Copies: {0}", this.NumCopies);
+		}
+	}
+
+	/// <summary>
+	/// The 'ConcreteComponent' class
+	/// </summary>
+	public class Video : LibraryItem
+	{
+		public string Director { get; set; }
+		public string Title { get; set; }
+		public int PlayTime { get; set; }
+
+		public Video(string director, string title, int numCopies, int playTime)
+		{
+			this.Director = director;
+			this.Title = title;
+			this.NumCopies = numCopies;
+			this.PlayTime = playTime;
+		}
+
+		public override void Display()
+		{
+			Console.WriteLine("\nVideo ----- ");
+			Console.WriteLine(" Director: {0}", this.Director);
+			Console.WriteLine(" Title: {0}", this.Title);
+			Console.WriteLine(" # Copies: {0}", this.NumCopies);
+			Console.WriteLine(" Playtime: {0}\n", this.PlayTime);
+		}
+	}
+
+	/// <summary>
+	/// The 'Decorator' abstract class
+	/// </summary>
+	public abstract class Decorator : LibraryItem
+	{
+		protected LibraryItem LibraryItem { get; private set; }
+
+		public Decorator(LibraryItem libraryItem)
+		{
+			this.LibraryItem = libraryItem;
+		}
+
+		public override void Display()
+		{
+			this.LibraryItem.Display();
+		}
+	}
+
+	/// <summary>
+	/// The 'ConcreteDecorator' class.
+	/// </summary>
+	public class Borrowable : Decorator
+	{
+		private List<string> borrowers = new List<string>();
+
+		protected List<string> Borrowers
+		{
+			get { return borrowers; }
+			set { borrowers = value; }
+		}
+
+		public Borrowable(LibraryItem libraryItem)
+			: base(libraryItem)
+		{
+		}
+
+		public void BorrowItem(string name)
+		{
+			this.Borrowers.Add(name);
+			this.LibraryItem.NumCopies--;
+		}
+
+		public void ReturnItem(string name)
+		{
+			this.Borrowers.Remove(name);
+			this.LibraryItem.NumCopies++;
+		}
+
+		public override void Display()
+		{
+			base.Display();
+			foreach (string borrower in this.Borrowers)
+			{
+				Console.WriteLine(" Borrower: " + borrower);
+			}
+		}
+	}
+}
