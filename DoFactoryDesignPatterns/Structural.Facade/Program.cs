8fdbb87e732ec1dd8705fac6e08d9c6a464d4016
iChrisJ <jiangl_8809@hotmail.com>
2014-11-02 23:28:05 +0800

    Examples for Facade pattern

---
+++ DoFactoryDesignPatterns/Structural.Facade/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Facade
+{
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	Facade facade = new Facade();
+		//	facade.MethodA();
+		//	facade.MethodB();
+
+		//	Console.ReadKey();
+		//}
+	}
+
+	/// <summary>
+	/// Subsystem classes   (Bank, Credit, Loan)
+	/// 1. implement subsystem functionality.
+	/// 2. handle work assigned by the Facade object.
+	/// 3. have no knowledge of the facade and keep no reference to it. 
+	/// </summary>
+	public class SubSystemOne
+	{
+		public void MethodOne()
+		{
+			Console.WriteLine(" SubSystemOne Method.");
+		}
+	}
+
+	public class SubSystemTwo
+	{
+		public void MeothodTwo()
+		{
+			Console.WriteLine(" SubSystemTwo Method.");
+		}
+	}
+
+	public class SubSystemThree
+	{
+		public void MethodThree()
+		{
+			Console.WriteLine(" SubSystemThree Method.");
+		}
+	}
+
+	public class SubSystemFour
+	{
+		public void MethodFour()
+		{
+			Console.WriteLine(" SubSystemFour Method.");
+		}
+	}
+
+	/// <summary>
+	/// Facade   (MortgageApplication)
+	/// 1. knows which subsystem classes are responsible for a request.
+	/// 2. delegates client requests to appropriate subsystem objects.
+	/// </summary>
+	public class Facade
+	{
+		private SubSystemOne _one;
+		private SubSystemTwo _two;
+		private SubSystemThree _three;
+		private SubSystemFour _four;
+
+		public Facade()
+		{
+			_one = new SubSystemOne();
+			_two = new SubSystemTwo();
+			_three = new SubSystemThree();
+			_four = new SubSystemFour();
+		}
+
+		public void MethodA()
+		{
+			Console.WriteLine("\nMethodA() ---- ");
+			_one.MethodOne();
+			_two.MeothodTwo();
+			_four.MethodFour();
+		}
+
+		public void MethodB()
+		{
+			Console.WriteLine("\nMethodB() ---- ");
+			_two.MeothodTwo();
+			_three.MethodThree();
+		}
+	}
+}
---
+++ DoFactoryDesignPatterns/Structural.Facade/RealWorld.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Facade
+{
+	class RealWorld
+	{
+		public static void Main()
+		{
+			Mortgage mortgage = new Mortgage();
+			Customer customer = new Customer("Ann Mckinsey");
+			bool eligible = mortgage.IsEligible(customer, 12500);
+
+			Console.WriteLine("\n" + customer.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'SubSystem ClassA' class.
+	/// </summary>
+	public class Bank
+	{
+		public bool HasSufficientSavings(Customer customer, int amount)
+		{
+			Console.WriteLine("Check bank for " + customer.Name);
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// The 'SubSystem ClassB' class.
+	/// </summary>
+	public class Credit
+	{
+		public bool HasGoodCredit(Customer customer)
+		{
+			Console.WriteLine("Check credit for " + customer.Name);
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// The 'SubSystem ClassC' class.
+	/// </summary>
+	public class Loan
+	{
+		public bool HasNoBadLoans(Customer customer)
+		{
+			Console.WriteLine("Check loan for " + customer.Name);
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// Customer class.
+	/// </summary>
+	public class Customer
+	{
+		public string Name { get; private set; }
+
+		public Customer(string name)
+		{
+			this.Name = name;
+		}
+	}
+
+	/// <summary>
+	/// The 'Facade' class.
+	/// </summary>
+	public class Mortgage
+	{
+		private Bank _bank = new Bank();
+		private Credit _credit = new Credit();
+		private Loan _loan = new Loan();
+
+		public bool IsEligible(Customer customer, int amount)
+		{
+			Console.WriteLine("{0} applies for {1:C} loan\n", customer.Name, amount);
+
+			return (!_bank.HasSufficientSavings(customer, amount)
+				|| !_loan.HasNoBadLoans(customer) ||
+				!_credit.HasGoodCredit(customer))
+				? false : true;
+		}
+	}
+}
