498b647d759799f37043b3bff7d3814c74516e59
ChrisJ <jiangl_8809@hotmail.com>
2014-11-03 14:48:32 +0800

    Examples for Flyweight, Proxy, Chain of Responsibility and Command patterns.

---
+++ DoFactoryDesignPatterns/Behavioral.ChainOfResponsibility/RealWorld.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.ChainOfResponsibility
+{
+	class RealWorld
+	{
+		static void Main()
+		{
+			Approver larry = new Director();
+			Approver sam = new VicePresident();
+			Approver tammy = new President();
+
+			larry.SetSuccessor(sam);
+			sam.SetSuccessor(tammy);
+
+			Purchase p = new Purchase(2034, 350.00, "Assets");
+			larry.ProcessRequest(p);
+
+			p = new Purchase(2035, 32590.00, "Project X");
+			larry.ProcessRequest(p);
+
+			p = new Purchase(2036, 122100.00, "Project Y");
+			larry.ProcessRequest(p);
+
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Handler' abstract class.
+	/// </summary>
+	public abstract class Approver
+	{
+		protected Approver successor;
+
+		public void SetSuccessor(Approver successor)
+		{
+			this.successor = successor;
+		}
+		public abstract void ProcessRequest(Purchase purchase);
+	}
+
+	/// <summary>
+	/// The 'ConcreteHandler' class.
+	/// </summary>
+	public class Director : Approver
+	{
+		public override void ProcessRequest(Purchase purchase)
+		{
+			if (purchase.Amount < 1000.0)
+			{
+				Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+			}
+			else if (successor != null)
+			{
+				successor.ProcessRequest(purchase);
+			}
+		}
+	}
+
+	public class VicePresident : Approver
+	{
+		public override void ProcessRequest(Purchase purchase)
+		{
+			if (purchase.Amount < 25000.0)
+			{
+				Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+			}
+			else if (successor != null)
+			{
+				successor.ProcessRequest(purchase);
+			}
+		}
+	}
+
+	public class President : Approver
+	{
+		public override void ProcessRequest(Purchase purchase)
+		{
+			if (purchase.Amount < 100000.0)
+			{
+				Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+			}
+			else
+			{
+				Console.WriteLine("Request# {0} requires an executive meeting!", purchase.Number);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Class holding request details.
+	/// </summary>
+	public class Purchase
+	{
+		private int _number;
+
+		public int Number
+		{
+			get { return _number; }
+			set { _number = value; }
+		}
+
+		private double _amount;
+
+		public double Amount
+		{
+			get { return _amount; }
+			set { _amount = value; }
+		}
+
+		private string _purpose;
+
+		public string Purpose
+		{
+			get { return _purpose; }
+			set { _purpose = value; }
+		}
+
+		public Purchase(int number, double amount, string purpose)
+		{
+			this.Number = number;
+			this.Amount = amount;
+			this.Purpose = purpose;
+		}
+	}
+}
---
+++ DoFactoryDesignPatterns/Structural.Proxy/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.Proxy
+{
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	Proxy proxy = new Proxy();
+		//	proxy.Request();
+
+		//	Console.ReadKey();
+		//}
+	}
+
+	/// <summary>
+	/// Subject   (IMath)
+	/// 1. defines the common interface for RealSubject and Proxy so that a Proxy can be used
+	///		anywhere a RealSubject is expected.
+	/// </summary>
+	public abstract class Subject
+	{
+		public abstract void Request();
+	}
+
+	/// <summary>
+	/// RealSubject (Math)
+	/// 1. defines the real object that the proxy represents.
+	/// </summary>
+	public class RealSubject : Subject
+	{
+		public override void Request()
+		{
+			Console.WriteLine("Called RealSubject.Request");
+		}
+	}
+
+	/// <summary>
+	/// Proxy (MathProxy)
+	/// 1. maintains a reference that lets the proxy access the real subject. Proxy may refer to a 
+	///		Subject if the RealSubject and Subject interfaces are the same.
+	///	2. provides an interface identical to Subject's so that a proxy can be subtituted for the
+	///		real subject.
+	///	3. controls access to the real subject and may be responsible for creating and deleting it.
+	///	4. other reponsiblities depend on the kind of proxy:
+	///		*remote proxies are reponsible for encoding a request and its arguments and for sending
+	///			sending the encoded request to the real subject in a different address space.
+	///		*virtual proxies may cache additional information about the real subject so that they can
+	///			postpone accessing it. For example, the ImageProxy from the Motivation caches the real
+	///			images' extent.
+	///		*protection proxies check that the caller has the access permissions required to perform a
+	///			request.
+	/// </summary>
+	public class Proxy : Subject
+	{
+		private RealSubject _realSubject;
+
+		public override void Request()
+		{
+			// Use "lazy initalization"
+			if (_realSubject == null)
+			{
+				_realSubject = new RealSubject();
+			}
+			_realSubject.Request();
+		}
+	}
+}
