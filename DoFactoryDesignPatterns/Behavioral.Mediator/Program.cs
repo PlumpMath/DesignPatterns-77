e00f2790449789e216c302337f3e6d5d91e8f0c0
ChrisJ <jiangl_8809@hotmail.com>
2014-11-04 15:16:25 +0800

    Example for Mediator pattern

---
+++ DoFactoryDesignPatterns/Behavioral.Mediator/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.Mediator
+{
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	ConcreteMediator m = new ConcreteMediator();
+
+		//	ConcreteColleague1 c1 = new ConcreteColleague1(m);
+		//	ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+		//	m.Colleague1 = c1;
+		//	m.Colleague2 = c2;
+
+		//	c1.Send("How are you?");
+		//	c2.Send("Fine, thank you.");
+
+		//	Console.ReadKey();
+		//}
+	}
+
+	/// <summary>
+	/// Mediator (IChatroom)
+	/// 1. defines an interface for communicating with Colleague objects.
+	/// </summary>
+	public abstract class Mediator
+	{
+		public abstract void Send(string message, Colleague colleague);
+	}
+
+	/// <summary>
+	/// ConcreteMediator (Chatroom)
+	/// 1. implements cooperative behavior by coordinating Colleague objects.
+	/// 2. knows and maintains its colleagues.
+	/// </summary>
+	public class ConcreteMediator : Mediator
+	{
+		private ConcreteColleague1 _colleague1;
+
+		public ConcreteColleague1 Colleague1
+		{
+			get { return _colleague1; }
+			set { _colleague1 = value; }
+		}
+
+		private ConcreteColleague2 _colleague2;
+
+		public ConcreteColleague2 Colleague2
+		{
+			get { return _colleague2; }
+			set { _colleague2 = value; }
+		}
+
+		public override void Send(string message, Colleague colleague)
+		{
+			if (colleague == this.Colleague1)
+			{
+				this.Colleague2.Notify(message);
+			}
+			else
+			{
+				this.Colleague1.Notify(message);
+			}
+		}
+	}
+
+	public abstract class Colleague
+	{
+		protected Mediator mediator;
+
+		public Colleague(Mediator mediator)
+		{
+			this.mediator = mediator;
+		}
+	}
+
+	/// <summary>
+	/// Colleague classes (Participant)
+	/// 1. each Colleague class knows its Mediator object.
+	/// 2. each colleague communicates with its mediator whenever it would have otherwise communicated
+	///		with another colleague.
+	/// </summary>
+	public class ConcreteColleague1 : Colleague
+	{
+		public ConcreteColleague1(Mediator mediator)
+			: base(mediator)
+		{
+		}
+
+		public void Send(string message)
+		{
+			mediator.Send(message, this);
+		}
+
+		public void Notify(string message)
+		{
+			Console.WriteLine("Colleague1 gets message: " + message);
+		}
+	}
+
+	public class ConcreteColleague2 : Colleague
+	{
+		public ConcreteColleague2(Mediator mediator)
+			: base(mediator)
+		{
+		}
+
+		public void Send(string message)
+		{
+			mediator.Send(message, this);
+		}
+
+		public void Notify(string message)
+		{
+			Console.WriteLine("Colleague2 gets message: " + message);
+		}
+	}
+}
---
+++ DoFactoryDesignPatterns/Behavioral.Mediator/RealWorld.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.Mediator
+{
+	class RealWorld
+	{
+		static void Main()
+		{
+			Chatroom chatroom = new Chatroom();
+
+			Participant George = new Beatle("George");
+			Participant Paul = new Beatle("Paul");
+			Participant Ringo = new Beatle("Ringo");
+			Participant John = new Beatle("John");
+			Participant Yoko = new NonBeatle("Yoko");
+
+			chatroom.Register(George);
+			chatroom.Register(Paul);
+			chatroom.Register(Ringo);
+			chatroom.Register(John);
+			chatroom.Register(Yoko);
+
+			Yoko.Send("John", "Hi John!");
+			Paul.Send("Ringo", "All you need is love");
+			Ringo.Send("George", "My sweet Lord");
+			Paul.Send("John", "Can't buy me love");
+			John.Send("Yoko", "My sweet love");
+
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Mediator' abstract class.
+	/// </summary>
+	public abstract class AbstractChatroom
+	{
+		public abstract void Register(Participant participant);
+
+		public abstract void Send(string from, string to, string message);
+	}
+
+	public class Chatroom : AbstractChatroom
+	{
+		private Dictionary<string, Participant> _participants = new Dictionary<string, Participant>();
+
+		public override void Register(Participant participant)
+		{
+			if (!_participants.ContainsValue(participant))
+			{
+				_participants[participant.Name] = participant;
+			}
+			participant.Chatroom = this;
+		}
+
+		public override void Send(string from, string to, string message)
+		{
+			Participant participant = _participants[to];
+			if (participant != null)
+			{
+				participant.Receive(from, message);
+			}
+		}
+	}
+
+	/// <summary>
+	/// The 'AbstractColleague' class.
+	/// </summary>
+	public class Participant
+	{
+		public string Name { get; private set; }
+
+		private AbstractChatroom _chatroom;
+
+		public AbstractChatroom Chatroom
+		{
+			get { return _chatroom; }
+			set { _chatroom = value; }
+		}
+
+		public Participant(string name)
+		{
+			this.Name = name;
+		}
+
+		public void Send(string to, string message)
+		{
+			this.Chatroom.Send(this.Name, to, message);
+		}
+
+		public virtual void Receive(string from, string message)
+		{
+			Console.WriteLine("{0} to {1}: '{2}'", from, this.Name, message);
+		}
+	}
+
+	/// <summary>
+	/// A 'ConcreteColleague' class.
+	/// </summary>
+	public class Beatle : Participant
+	{
+		public Beatle(string name)
+			: base(name)
+		{
+		}
+
+		public override void Receive(string from, string message)
+		{
+			Console.Write("To a Beatle: \t");
+			base.Receive(from, message);
+		}
+	}
+
+	public class NonBeatle : Participant
+	{
+		public NonBeatle(string name)
+			: base(name)
+		{
+		}
+
+		public override void Receive(string from, string message)
+		{
+			Console.Write("To a non-Beatle: \t");
+			base.Receive(from, message);
+		}
+	}
+}
