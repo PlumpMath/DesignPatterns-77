b96fdfd64719e2ef6419d27378f28a41d188757f
iChrisJ <jiangl_8809@hotmail.com>
2014-11-15 21:49:14 +0800

    Example for Factory Method pattern

---
+++ DoFactoryDesignPatterns/Creational.FactoryMethod/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Define an interface for creating an object, but let subclasses decide which class to instatiate.
+ * Factory Method lets a class defer instantiation to subclasses.
+ */
+namespace Creational.FactoryMethod
+{
+	using System;
+
+	//class Program
+	//{
+	//	static void Main(string[] args)
+	//	{
+	//		Creator[] creators = new Creator[2];
+
+	//		creators[0] = new ConcreteCreatorA();
+	//		creators[1] = new ConcreteCreatorB();
+
+	//		foreach (Creator creator in creators)
+	//		{
+	//			Product product = creator.FactoryMethod();
+	//			Console.WriteLine("Created {0}", product.GetType().Name);
+	//		}
+
+	//		Console.ReadKey();
+	//	}
+	//}
+
+	/// <summary>
+	/// Product (Page)
+	/// 1. defines the interface of objects the factory method creates.
+	/// </summary>
+	public abstract class Product
+	{
+	}
+
+	/// <summary>
+	/// ConcreteProduct (SkillsPage, EducationPage, ExperiencePage)
+	/// 1. implements the Product interface.
+	/// </summary>
+	public class ConcreteProductA : Product
+	{
+	}
+
+	public class ConcreteProductB : Product
+	{
+	}
+
+	/// <summary>
+	/// Creator (Document)
+	/// 1. declares the factory method, which returns an object of type Product. Creator may also
+	///		define a default implementation of the factory method that returns a default ConcreteProduct
+	///		object.
+	/// </summary>
+	public abstract class Creator
+	{
+		public abstract Product FactoryMethod();
+	}
+
+	/// <summary>
+	/// ConcreteCreator (Report, Resume)]
+	/// 1. overrides the factory method to return an instance of a ConcreteProduct.
+	/// </summary>
+	public class ConcreteCreatorA : Creator
+	{
+		public override Product FactoryMethod()
+		{
+			return new ConcreteProductA();
+		}
+	}
+
+	public class ConcreteCreatorB : Creator
+	{
+		public override Product FactoryMethod()
+		{
+			return new ConcreteProductB();
+		}
+	}
+
+}
---
+++ DoFactoryDesignPatterns/Creational.FactoryMethod/RealWorld.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.FactoryMethod
+{
+	class RealWorld
+	{
+		static void Main()
+		{
+			// Note: constructors call Factory Method
+			Document[] documents = new Document[2];
+
+			documents[0] = new Resume();
+			documents[1] = new Report();
+
+			// Display document pages
+			foreach (Document document in documents)
+			{
+				Console.WriteLine("\n" + document.GetType().Name + "--");
+				foreach (Page page in document.Pages)
+				{
+					Console.WriteLine(" " + page.GetType().Name);
+				}
+			}
+
+			// Wait for user
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Product' abstract class.
+	/// </summary>
+	public abstract class Page
+	{
+	}
+
+	/// <summary>
+	/// A 'ConcreteProduct' class.
+	/// </summary>
+	public class SkillsPage : Page
+	{
+	}
+
+	public class EducationPage : Page
+	{
+	}
+
+	public class ExperiencePage : Page
+	{
+	}
+
+	public class IntroductionPage : Page
+	{
+	}
+
+	public class ResultsPage : Page
+	{
+	}
+
+	public class ConclusionPage : Page
+	{
+	}
+
+	public class SummaryPage : Page
+	{
+	}
+
+	public class BibliographyPage : Page
+	{
+	}
+
+	/// <summary>
+	/// The 'Creator' abstract class.
+	/// </summary>
+	public abstract class Document
+	{
+		private List<Page> _pages = new List<Page>();
+		public List<Page> Pages
+		{
+			get { return _pages; }
+		}
+
+		public Document()
+		{
+			this.CreatePages();
+		}
+
+		public abstract void CreatePages();
+	}
+
+	/// <summary>
+	/// A 'ConcreteCreator' Page.
+	/// </summary>
+	public class Resume : Document
+	{
+		public override void CreatePages()
+		{
+			this.Pages.Add(new SkillsPage());
+			this.Pages.Add(new EducationPage());
+			this.Pages.Add(new ExperiencePage());
+		}
+	}
+
+	public class Report : Document
+	{
+		public override void CreatePages()
+		{
+			this.Pages.Add(new IntroductionPage());
+			this.Pages.Add(new ResultsPage());
+			this.Pages.Add(new ConclusionPage());
+			this.Pages.Add(new SummaryPage());
+			this.Pages.Add(new BibliographyPage());
+		}
+	}
+
+}
