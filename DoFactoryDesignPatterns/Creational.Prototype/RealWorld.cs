8203c6d3da6b5093584db30af75facf6a9125b4a
iChrisJ <jiangl_8809@hotmail.com>
2014-11-17 22:17:11 +0800

    Examples for Prototype and Singleton patterns

---
+++ DoFactoryDesignPatterns/Creational.Prototype/RealWorld.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Prototype
+{
+	class RealWorld
+	{
+		static void Main()
+		{
+			ColorManager colorManager = new ColorManager();
+
+			colorManager["red"] = new Color(255, 0, 0);
+			colorManager["green"] = new Color(0, 255, 0);
+			colorManager["blue"] = new Color(0, 0, 255);
+
+			colorManager["angry"] = new Color(255, 54, 0);
+			colorManager["peace"] = new Color(128, 211, 128);
+			colorManager["flame"] = new Color(211, 34, 20);
+
+			Color color1 = colorManager["red"].Clone() as Color;
+			Color color2 = colorManager["peace"].Clone() as Color;
+			Color color3 = colorManager["flame"].Clone() as Color;
+
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// The 'Prototype' abstract class.
+	/// </summary>
+	public abstract class ColorPrototype
+	{
+		public abstract ColorPrototype Clone();
+	}
+
+	/// <summary>
+	/// The 'ConcretePrototype' class.
+	/// </summary>
+	public class Color : ColorPrototype
+	{
+		private int _red;
+		private int _green;
+		private int _blue;
+
+		public Color(int red, int green, int blue)
+		{
+			this._red = red;
+			this._green = green;
+			this._blue = blue;
+		}
+
+		public override ColorPrototype Clone()
+		{
+			Console.WriteLine("Cloning color RGB: {0,3},{1,3},{2,3}", this._red, this._green, this._blue);
+			return this.MemberwiseClone() as ColorPrototype;
+		}
+	}
+
+	/// <summary>
+	/// Prototype manager.
+	/// </summary>
+	public class ColorManager
+	{
+		private Dictionary<string, ColorPrototype> _colors = new Dictionary<string, ColorPrototype>();
+
+		public ColorPrototype this[string index]
+		{
+			get { return _colors[index]; }
+			set { _colors.Add(index, value); }
+		}
+	}
+}
---
+++ DoFactoryDesignPatterns/Creational.Singleton/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Ensure a class has only one instance and provide a global point of access to it.
+ */
+namespace Creational.Singleton
+{
+	using System;
+
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	Singleton s1 = Singleton.Instance();
+		//	Singleton s2 = Singleton.Instance();
+
+		//	if (s1 == s2)
+		//	{
+		//		Console.WriteLine("Objects are the same instance");
+		//	}
+
+		//	Console.ReadKey();
+		//}
+	}
+
+	/// <summary>
+	/// Singleton (LoadBalancer)
+	/// 1. defines an Instance operation that lets clients access its unique instance. Instance is a class operation.
+	/// 2. responsible for creating and maintaining its own unique instance.
+	/// </summary>
+	public class Singleton
+	{
+		private static Singleton _instance;
+
+		protected Singleton()
+		{
+		}
+
+		public static Singleton Instance()
+		{
+			// Uses lazy initialization
+			// Note: this is not thread safe.
+			if (Singleton._instance == null)
+			{
+				Singleton._instance = new Singleton();
+			}
+			return Singleton._instance;
+		}
+	}
+}
